c66811f847ecfcacd64ae5977afb3e95a14e4820
Lloyd <lloydmeta@gmail.com>
2014-08-27 21:39:54 +0900

    Better tests

--- src/test/scala/scala/ext/String/splitwith/SplitWithOpsSpec.scala
+++ src/test/scala/scala/ext/String/splitwith/SplitWithOpsSpec.scala
@@ -5,13 +5,35 @@ import org.scalatest.{Matchers, FunSpec}
 class SplitWithOpsSpec extends FunSpec with Matchers {
 
   describe("#splitWith") {
-    it("should split properly") {
-      "".splitWith("hi") should be (Seq.empty)
-      "".splitWith('.') should be (Seq.empty)
-      "hi".splitWith("hi") should be (Seq(""))
-      ".".splitWith('.') should be (Seq(""))
-      "hello.world.yay".splitWith("ello") should be (Seq("h", ".world.yay"))
-      "hello.world.yay".splitWith('.') should be(List("hello", "world", "yay"))
+
+    describe("char splitter") {
+
+      it("should return Nil for an empty string") {
+        "".splitWith('.') should be (Seq.empty)
+
+      }
+      it("should return a sequence with an empty string when the string contains just the separator"){
+        ".".splitWith('.') should be (Seq(""))
+      }
+      it("should properly manage regexp chars"){
+        "hello.world.yay".splitWith('.') should be(List("hello", "world", "yay"))
+      }
+
+    }
+
+    describe("string splitter") {
+      it("should return Nil for an empty string") {
+        "".splitWith("hi") should be (Seq.empty)
+      }
+      it("should return a sequence with an empty string when the string contains just the separator"){
+        "hi".splitWith("hi") should be (Seq(""))
+      }
+      it("should properly manage regexp chars in the separator string"){
+        "hello.world.yay".splitWith(".w") should be(List("hello", "orld.yay"))
+      }
+      it("should work with a simple separator string") {
+        "hello.world.yay".splitWith("ello") should be (Seq("h", ".world.yay"))
+      }
     }
   }
 
@@ -24,12 +46,11 @@ class SplitWithOpsSpec extends FunSpec with Matchers {
       System.currentTimeMillis() - start
     }
 
-    it("results") {
+    it("should be faster than normal string split") {
       val stringToSplit = "hello.there.you.me.him.e.haha.ehehe"
       val normalSplit = bench(stringToSplit.split("e"))
       val splitWith = bench(stringToSplit.splitWith("e"))
-      println(s"normal #split took: $normalSplit")
-      println(s"splitWith took: $splitWith")
+      splitWith should be < normalSplit
     }
 
   }
